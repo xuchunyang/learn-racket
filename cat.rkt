5db3f61b9158155beb40d958f915efecfba11e7a
Xu Chunyang <mail@xuchunyang.me>
2018-07-07 10:36:34 +0800

    * cat.rkt: Add test.

--- cat.rkt
+++ cat.rkt
@@ -4,3 +4,11 @@
   (unless (eof-object? b)
     (write-byte b)
     (rw (read-byte))))
+
+;; raco test cat.rkt
+(module test racket/base
+  (require rackunit
+           racket/port
+           racket/system)
+  (check-equal? (with-output-to-string (lambda () (system "cal")))
+                (with-output-to-string (lambda () (system "cal | racket cat.rkt")))))
