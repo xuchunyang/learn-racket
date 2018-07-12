a46963a2aa2d4ccf503d4be288d009a32b78ce74
Xu Chunyang <mail@xuchunyang.me>
2018-07-12 11:38:50 +0800

    * ls.rkt: Use in-directory instead of directory-list.
    速度更快，边遍历边打印

--- ls.rkt
+++ ls.rkt
@@ -1,11 +1,16 @@
-#lang racket
+#lang racket/base
 
 (define (ls file-or-dir)
   (if (file-exists? file-or-dir)
       (displayln file-or-dir)
       (for-each ls (directory-list file-or-dir #:build? #t))))
 
+(define (ls2 dir)
+  (for ([p (in-directory dir)])
+    (printf "~a\n" p)))
+
 (module+ main
+  (require racket/match)
   (match (current-command-line-arguments)
-    [(vector file-or-dir) (ls file-or-dir)]
-    [_ (ls (current-directory))]))
+    [(vector file-or-dir) (ls2 file-or-dir)]
+    [_ (ls2 (current-directory))]))
