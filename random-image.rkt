6e4b7905193220e103dfe591b1abbf2d0a7cb597
Xu Chunyang <mail@xuchunyang.me>
2018-07-14 02:15:32 +0800

    * random-image.rkt: New file.

---
+++ random-image.rkt
@@ -0,0 +1,33 @@
+#lang racket
+(require html-parsing
+         racket/random
+         sxml
+         web-server/servlet
+         web-server/servlet-env
+         "http-proxy.rkt")
+
+(define (extract-image-urls url)
+  (define-values (status headers html) (http-proxy-sendrecv/url (string->url url)))
+  (define xexp (html->xexp html))
+  (map cadr ((sxpath "//img/@src") xexp)))
+
+(define image-urls (extract-image-urls
+                    "https://onejav.com/"
+                    #;"https://en.wikipedia.org/wiki/Main_Page"))
+
+(define (start req)
+  (response/xexpr
+   `(html (head (title "Random Image"))
+          (body (img ([src ,(random-ref image-urls)]))))))
+
+(match (system-type)
+  ['macosx (serve/servlet start
+                          #:servlet-regexp #rx""
+                          #:servlet-path "/")]
+  ;; Ubuntu 服务器
+  ['unix (serve/servlet start
+                        #:servlet-regexp #rx""
+                        #:servlet-path "/"
+                        #:launch-browser? #f
+                        #:port 80
+                        #:listen-ip #f)])
