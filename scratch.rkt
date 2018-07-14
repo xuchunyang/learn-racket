e921c54dfcb354df193f47426508b14c51b4e415
Xu Chunyang <mail@xuchunyang.me>
2018-07-14 18:02:16 +0800

    * scratch.rkt: Send data to server with HTML <form>

--- scratch.rkt
+++ scratch.rkt
@@ -1,17 +1,37 @@
 #lang racket
 (require web-server/servlet
          web-server/servlet-env)
- 
+
+(define contents "some text here")
+
+(define (render-page request)
+  (define (response-generator embed/url)
+    (response/xexpr
+     `(html
+       (head (title "*scratch*")
+             (link ((rel "stylesheet")
+                    (href "/scratch.css")
+                    (type "text/css"))))
+       (body
+        (form ((action ,(embed/url save-textarea-handler)))
+              (textarea ((name "textarea")
+                         (rows "20")
+                         (cols "40"))
+                        ,contents)
+              (input ((type "submit")
+                      (value "Save"))))))))
+  (define (save-textarea-handler request)
+    (set! contents
+          (string-append (extract-binding/single 'textarea (request-bindings request))
+                         "\n"
+                         (string-trim
+                          (with-output-to-string (lambda () (system "date"))))))
+    (render-page (redirect/get)))
+  (send/suspend/dispatch response-generator))
+
 (define (start request)
-  (response/xexpr
-   '(html
-     (head (title "*scratch*")
-           (link ((rel "stylesheet")
-                  (href "/scratch.css")
-                  (type "text/css"))))
-     (body
-      ;; XXX 怎么获得这里的数据, JavaScript ?
-      (textarea ((rows "20") (cols "40")) "some text here")))))
+  (render-page request))
+
 
 (serve/servlet start
                #:extra-files-paths (list (current-directory)))
