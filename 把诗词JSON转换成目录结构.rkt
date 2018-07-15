ce132191fdc7497a2877b6e08b39fec90f7a1649
Xu Chunyang <mail@xuchunyang.me>
2018-07-15 21:49:13 +0800

    * 把诗词JSON转换成目录结构.rkt: 支持词

--- 把诗词JSON转换成目录结构.rkt
+++ 把诗词JSON转换成目录结构.rkt
@@ -21,28 +21,50 @@
              title author)]
     [else
      (let ([filename (build-path author title)])
-       (make-directory* author)
-       (with-output-to-file filename #:exists 'replace
-         (lambda () (map displayln paragraphs))))]))
+       (unless (file-exists? filename)
+         (make-directory* author)
+         (with-output-to-file filename
+           (lambda () (map displayln paragraphs)))))]))
 
 (define (save-hash h)
   (save-poet (hash-ref h 'author)
              (hash-ref h 'title)
              (hash-ref h 'paragraphs)))
 
+(define (save-hash-ci h)
+  (save-poet (hash-ref h 'author)
+             ;; 词牌名
+             (hash-ref h 'rhythmic)
+             (hash-ref h 'paragraphs)))
+
 (module+ main
-  (define DIR (expand-user-path "~/src/chinese-poetry/json/"))
-  (unless (directory-exists? DIR)
-    (error 'foo "目录 ~a 不存在，请从 https://github.com/chinese-poetry/chinese-poetry 下载" DIR))
   (unless (directory-exists? "诗词")
     (make-directory "诗词"))
   (current-directory "诗词")
-  (for ([f (in-directory DIR)])
-    (when (regexp-match
-           ;; poet.song.25000.json
-           ;; poet.tang.8000.json
-           "poet\\.(?:tang|song).+\\.json"
-           (path->string f))
-      (printf "Processing ~a\n" f)
-      (for ([h (in-list (extract-json f))])
-        (save-hash h)))))
+
+  ;; 诗
+  (let ([DIR (expand-user-path "~/src/chinese-poetry/json/")])
+    (unless (directory-exists? DIR)
+      (error 'foo "目录 ~a 不存在，请从 https://github.com/chinese-poetry/chinese-poetry 下载" DIR))
+    (for ([f (in-directory DIR)])
+      (when (regexp-match
+             ;; poet.song.25000.json
+             ;; poet.tang.8000.json
+             "poet\\.(?:tang|song).+\\.json"
+             (path->string f))
+        (printf "Processing ~a\n" f)
+        (for ([h (in-list (extract-json f))])
+          (save-hash h)))))
+
+  ;; 词
+  (let ([DIR (expand-user-path "~/src/chinese-poetry/ci/")])
+    (unless (directory-exists? DIR)
+      (error 'foo "目录 ~a 不存在，请从 https://github.com/chinese-poetry/chinese-poetry 下载" DIR))
+    (for ([f (in-directory DIR)])
+      (when (regexp-match
+             ;; ci.song.0.json
+             "ci\\.(?:tang|song).+\\.json"
+             (path->string f))
+        (printf "Processing ~a\n" f)
+        (for ([h (in-list (extract-json f))])
+          (save-hash-ci h))))))
